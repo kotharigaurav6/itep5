c9bb6e15dd86f7e2349b1b96dde2fb7d63754a1f
Gaurav Kothari <87779512+kotharigaurav6@users.noreply.github.com>
2023-09-29 16:02:40 +0530

    Add files via upload

--- Mern_stack/ExpressJS/7 Express with mysql/controller/adminController.js
+++ Mern_stack/ExpressJS/7 Express with mysql/controller/adminController.js
@@ -2,18 +2,67 @@ import connectInstance from '../model/connection.js';
 
 export const adminViewUserController = (request, response) => {
     var sql = "select * from user";
-    connectInstance.query(sql,(err, result) => {
+    connectInstance.query(sql, (err, result) => {
         if (err)
             console.log("error while fetching user data");
         else {
-            response.render("adminviewuser",{userrecord : result,adminemail:""});
+            response.render("adminviewuser", { userrecord: result, adminemail: request.session.adminemail });
         }
     });
 }
 
-
 export const adminLogoutController = (request, response) => {
-                request.session.adminemail = "";
-                request.session.destroy();
-                response.redirect("/");
-    }
+    request.session.adminemail = "";
+    request.session.destroy();
+    response.redirect("/");
+}
+
+export const adminUpdateUserController = (request, response) => {
+    const email = request.params.email;
+    var sql = `select * from user where email="${email}"`;
+    // console.log(connectInstance);
+    connectInstance.query(sql, (err, result) => {
+        if (err)
+            console.log("error while fetching data");
+        else {
+            response.render("adminUpdateUser", { adminemail: request.session.adminemail, result:result[0]});
+        }
+    });
+}
+
+export const adminUpdateDataController = (request, response) => {
+    const { username, email, password, address } = request.body;
+    var sql = `update user set username="${username}",password="${password}",address="${address}" where email="${email}"`;
+   // console.log(connectInstance);
+    connectInstance.query(sql, (err, result) => {
+        if (err)
+            console.log("error while updating data");
+        else {
+            console.log("Data updated successfully");
+            response.render("adminhome",{adminemail:request.session.adminemail});
+        }
+    });
+}
+
+export const adminDeleteUserController = (request, response) => {
+    const email = request.params.email;
+    var sql = `delete from user where email="${email}"`;
+   // console.log(connectInstance);
+    connectInstance.query(sql, (err, result) => {
+        if (err)
+            console.log("error while updating data");
+        else {
+            console.log("Data deleted successfully");
+            /* user list start */
+            var sql = "select * from user";
+            connectInstance.query(sql, (err, result) => {
+                if (err)
+                    console.log("error while fetching user data");
+                else {
+                    response.render("adminviewuser", { userrecord: result, adminemail: request.session.adminemail });
+                }
+            });
+            /* user list end */
+        }
+    });
+}
--- Mern_stack/ExpressJS/7 Express with mysql/router/adminRoute.js
+++ Mern_stack/ExpressJS/7 Express with mysql/router/adminRoute.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import {adminViewUserController,adminLogoutController} from '../controller/adminController.js';
+import {adminViewUserController,adminLogoutController,adminUpdateUserController,adminUpdateDataController,adminDeleteUserController} from '../controller/adminController.js';
 
 var adminRouter = express.Router();
 
 adminRouter.get("/viewuser",adminViewUserController);
+adminRouter.get("/updateuser/:email",adminUpdateUserController);
+adminRouter.post("/updatedata",adminUpdateDataController);
+adminRouter.get("/deleteuser/:email",adminDeleteUserController);
 adminRouter.get("/logout",adminLogoutController);
 
 export default adminRouter;

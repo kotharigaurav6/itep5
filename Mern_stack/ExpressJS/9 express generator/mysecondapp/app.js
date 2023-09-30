62d0f70c30966c72fe530488d2adf0cf1ea3053d
Gaurav Kothari <87779512+kotharigaurav6@users.noreply.github.com>
2023-09-30 14:54:30 +0530

    Add files via upload

---
+++ Mern_stack/ExpressJS/9 express generator/mysecondapp/app.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+module.exports = app;
